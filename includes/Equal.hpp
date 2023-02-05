bad243d8a73034e0b9ca9ac7e51b517d11476d77
root <root@LAPTOP-QTQ73EU2.localdomain>
2023-02-05 20:14:58 +0100

    pair done + equal done

--- includes/Equal.hpp
+++ includes/Equal.hpp
@@ -3,12 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   Equal.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chaidel <chaidel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:59:35 by chaidel           #+#    #+#             */
-/*   Updated: 2023/01/30 17:59:41 by chaidel          ###   ########.fr       */
+/*   Updated: 2023/02/05 19:09:09 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+template <class InputIterator1, class InputIterator2>
+  bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2 )
+{
+  while (first1 != last1)
+  {
+	if (!(*first1 == *first2))
+		return (false);
+	++first1;
+	++first2;
+  }
+  return (true);
+}
 
--- includes/pair.hpp
+++ includes/pair.hpp
@@ -22,7 +22,67 @@ namespace ft
 		// Member first is constructed with a and member second with b.
 		pair (const first_type& a, const second_type& b) : first(a), second(b) {}
 
+
+		pair& operator= (const pair& pr)
+		{
+			if (this != &pr)
+			{
+				first = pr.first;
+				second = pr.second;
+			}
+			return (*this);
+		}
+
 		_T1 first;                 // @c first is a copy of the first object
 		_T2 second;                // @c second is a copy of the second object
 	};
+
+	template <class T1, class T2>
+	pair<T1, T2> make_pair (T1 x, T2 y)
+	{
+		return (pair<T1, T2>(x, y));
+	}
+
+
+	// void	swap(pair& pr)
+	// {
+	// 	std::swap(first, pr.first);
+	// 	std::swap(second, pr.second);
+	// }
+
+	template<class T>
+	void	swap(T&	a, T&b)
+	{
+		T	c(a);
+		a = b;
+		b = c;
+	}
+
+
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{	return (lhs.fisrt == rhs.first && lhs.second == rhs.second); }
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{	return !(lhs == rhs); }
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{	return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second)); }
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{	return (!(rhs < lhs)) }
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{	return (rhs < lhs); }
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{	return (!(lhs < rhs)); }
+
+
+
 }
